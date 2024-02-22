f34ed4f2875c874da770330dcbdd36b110f3aa14
Centauri <centauri2442@gmail.com>
2024-02-21 20:17:20 -0600

    Initial release

---
+++ Packages/centauricore.masterchecker/README.md
@@ -0,0 +1,23 @@
+INFO -------------------------------------------------------------
+
+MasterFailureCheck is a drag and drop prefab that allows developers to easily detect if players in the instance have lost their network connection to VRChat, allowing them to then change ownership of objects as needed.
+
+SETUP -------------------------------------------------------------
+
+1) Drag and drop the "Failure Checker" prefab into your scene (Make sure to only have 1!)
+2) Assign any behaviours you want the "OnMasterFailure" event to fire on into the array
+
+CHANGELOG -------------------------------------------------------------
+
+v1.0
+- Initial Release
+
+LICENSING -------------------------------------------------------------
+
+Copyright 2024 CentauriCore
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ Packages/centauricore.masterchecker/Runtime/Scripts/FailureChecker.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Centauri.FailureChecking
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class FailureChecker : UdonSharpBehaviour
+    {
+        [Tooltip("Failure threshold (s)")]
+        public int FailureThreshold = 10;
+        private int currentValue = 0;
+        private bool hasFailed;
+
+        [UdonSynced] private byte serializeTestVal;
+        
+        [Tooltip("All behaviours that the OnMasterFailure event is fired on!")]
+        public UdonSharpBehaviour[] AllAttachedBehaviours = new UdonSharpBehaviour[0];
+        private string FailureEventName = "OnMasterFailure";
+        
+        
+        private void Start()
+        {
+            SendCustomEventDelayedSeconds(nameof(CheckLoop), 1f);
+        }
+
+        public void CheckLoop()
+        {
+            if (Networking.IsOwner(gameObject))
+            {
+                SerializeData();
+            }
+            else
+            {
+                currentValue+= 2;
+                
+                if (!hasFailed && currentValue >= FailureThreshold)
+                {
+                    foreach (var behaviour in AllAttachedBehaviours)
+                    {
+                        behaviour.SendCustomEvent(FailureEventName);
+                    }
+
+                    hasFailed = true;
+                }
+            }
+            
+            SendCustomEventDelayedSeconds(nameof(CheckLoop), 2);
+        }
+
+        public void OnMasterFailure()
+        {
+            Debug.Log("[<color=red>FAILURE CHECK</color>] Master has failed!");
+        }
+
+        #region Networking
+
+        public void SerializeData()
+        {
+            if (VRCPlayerApi.GetPlayerCount() < 2)
+            {
+                OnPreSerialization();
+            }
+            
+            RequestSerialization();
+        }
+
+        public override void OnPreSerialization()
+        {
+            currentValue = 0;
+        }
+
+        public override void OnDeserialization()
+        {
+            currentValue = 0;
+        }
+
+        public override void OnOwnershipTransferred(VRCPlayerApi player)
+        {
+            currentValue = 0;
+        }
+
+        #endregion
+    }
+}
